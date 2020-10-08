b0810c31cb6efffb32f14805ca2be7d3e49557ae
Plimon Howard <plimon.howard@gmail.com>
2020-10-08 20:09:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -129,7 +129,7 @@ class TicTacToe
        turn
      end
      if won?
-       puts "Congratulations #{winner}"
+       puts "Congratulations #{winner}!"
      elsif draw?
        puts "Cat's Game!"
      end
